01ac77ed037e036b6840715244145f6a8cfc9143
Caio Tokunaga <caiormt@gmail.com>
2024-06-23 11:56:05 -0400

    feat: convert byte

--- src/main/java/br/com/tokunaga/trancoder/Trancoder.java
+++ src/main/java/br/com/tokunaga/trancoder/Trancoder.java
@@ -32,6 +32,11 @@ public abstract class Trancoder {
     return safePadValue(str, size, padChar, leftPad);
   }
 
+  public static String convert(final Byte value, final int size, final char padChar, final boolean leftPad) {
+    final String str = safeValue(value);
+    return safePadValue(str, size, padChar, leftPad);
+  }
+
   private static String safePadValue(final String str, final int size, final char padChar, final boolean leftPad) {
     final String value = padValue(str, size, padChar, leftPad);
     if (isPadOverflow(value, size))
@@ -61,5 +66,9 @@ public abstract class Trancoder {
 
   private static String safeValue(final Short value) {
     return Objects.nonNull(value) ? Short.toString(value) : StringUtils.EMPTY;
+  }
+
+  private static String safeValue(final Byte value) {
+    return Objects.nonNull(value) ? Byte.toString(value) : StringUtils.EMPTY;
   }
 }
