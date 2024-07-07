f8759c902624862fb4b2eb63bd0d4528831af476
Caio Tokunaga <caiormt@gmail.com>
2024-07-07 11:36:06 -0400

    feat: convert float null as zero

--- src/main/java/br/com/tokunaga/trancoder/Trancoder.java
+++ src/main/java/br/com/tokunaga/trancoder/Trancoder.java
@@ -98,8 +98,14 @@ public abstract class Trancoder {
     return safePadValue(str, size, padChar, leftPad);
   }
 
-  public static String convert(final Float value, final int size, final char padChar, final boolean leftPad) {
-    final String str = safeValue(value);
+  public static String convert(
+      final Float value,
+      final int size,
+      final char padChar,
+      final boolean leftPad,
+      final boolean zeroIfNull) {
+
+    final String str = safeValue(value, numericNullDefault(zeroIfNull));
     return safePadValue(str, size, padChar, leftPad);
   }
 
