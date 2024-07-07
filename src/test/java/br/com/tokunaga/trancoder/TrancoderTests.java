30217d4f96c1d5ef207f6791168b28ddbaac537a
Caio Tokunaga <caiormt@gmail.com>
2024-07-07 11:33:06 -0400

    test: convert double null as zero

--- src/test/java/br/com/tokunaga/trancoder/TrancoderTests.java
+++ src/test/java/br/com/tokunaga/trancoder/TrancoderTests.java
@@ -146,8 +146,21 @@ class TrancoderTests {
       @ForAll final char padChar) {
 
     String pad = StringUtils.repeat(padChar, size);
-    assertThat(Trancoder.convert((Double) null, size, padChar, false))
+    assertThat(Trancoder.convert((Double) null, size, padChar, false, false))
         .isEqualTo(pad);
+  }
+
+  @Property
+  void shouldConvertNullDoubleAsZero(
+      @ForAll @IntRange(min = 1, max = 1000) final int size,
+      @ForAll final char padChar) {
+
+    String pad = StringUtils.repeat(padChar, size - 1);
+    assertThat(Trancoder.convert((Double) null, size, padChar, false, true))
+        .isEqualTo(ZERO + pad);
+
+    assertThat(Trancoder.convert((Double) null, size, padChar, true, true))
+        .isEqualTo(pad + ZERO);
   }
 
   @Property
@@ -308,7 +321,7 @@ class TrancoderTests {
     String expected = Double.toString(value);
     int length = expected.length();
     String pad = StringUtils.repeat(padChar, size);
-    assertThat(Trancoder.convert(value, length + size, padChar, false))
+    assertThat(Trancoder.convert(value, length + size, padChar, false, false))
         .isEqualTo(expected + pad);
   }
 
@@ -462,7 +475,7 @@ class TrancoderTests {
     String expected = Double.toString(value);
     int length = expected.length();
     String pad = StringUtils.repeat(padChar, size);
-    assertThat(Trancoder.convert(value, length + size, padChar, true))
+    assertThat(Trancoder.convert(value, length + size, padChar, true, false))
         .isEqualTo(pad + expected);
   }
 
@@ -662,10 +675,16 @@ class TrancoderTests {
     String expected = Double.toString(value);
     int length = expected.length();
     int target = length - size;
-    assertThatThrownBy(() -> Trancoder.convert(value, target, padChar, true))
+    assertThatThrownBy(() -> Trancoder.convert(value, target, padChar, true, false))
         .isExactlyInstanceOf(TrancodeOverflowException.class);
 
-    assertThatThrownBy(() -> Trancoder.convert(value, target, padChar, false))
+    assertThatThrownBy(() -> Trancoder.convert(value, target, padChar, false, false))
+        .isExactlyInstanceOf(TrancodeOverflowException.class);
+
+    assertThatThrownBy(() -> Trancoder.convert(value, target, padChar, true, true))
+        .isExactlyInstanceOf(TrancodeOverflowException.class);
+
+    assertThatThrownBy(() -> Trancoder.convert(value, target, padChar, false, true))
         .isExactlyInstanceOf(TrancodeOverflowException.class);
   }
 
