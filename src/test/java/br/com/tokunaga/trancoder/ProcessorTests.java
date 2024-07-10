a8bc4270510c8bcd3b48e0d01a07a76d9b49bac9
Caio Tokunaga <caiormt@gmail.com>
2024-07-09 22:24:25 -0400

    test: annotation processor

---
+++ src/test/java/br/com/tokunaga/trancoder/ProcessorTests.java
@@ -0,0 +1,83 @@
+package br.com.tokunaga.trancoder;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.apache.commons.lang3.StringUtils;
+
+import net.jqwik.api.Example;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.StringLength;
+
+import br.com.tokunaga.trancoder.util.StringHolderLeft;
+import br.com.tokunaga.trancoder.util.StringHolderLeftNull;
+import br.com.tokunaga.trancoder.util.StringHolderRight;
+import br.com.tokunaga.trancoder.util.StringHolderRightNull;
+
+class ProcessorTests {
+
+  @Example
+  void shouldTrancodeNullObject() {
+    assertThat(Processor.trancode(null))
+        .isEqualTo("");
+  }
+
+  @Property
+  void shouldTrancodeStringField(@ForAll @StringLength(100) final String string) {
+    final StringHolderRight stringHolder = new StringHolderRight(string);
+    assertThat(Processor.trancode(stringHolder))
+        .isEqualTo(string);
+  }
+
+  @Property
+  void shouldTrancodeStringFieldRightPadding(@ForAll @StringLength(max = 100) final String string) {
+    final int length = string.length();
+    final String pad = StringUtils.repeat(' ', 100 - length);
+    final StringHolderRight stringHolder = new StringHolderRight(string);
+    assertThat(Processor.trancode(stringHolder))
+        .isEqualTo(string + pad);
+  }
+
+  @Property
+  void shouldTrancodeStringFieldLeftPadding(@ForAll @StringLength(max = 100) final String string) {
+    final int length = string.length();
+    final String pad = StringUtils.repeat(' ', 100 - length);
+    final StringHolderLeft stringHolder = new StringHolderLeft(string);
+    assertThat(Processor.trancode(stringHolder))
+        .isEqualTo(pad + string);
+  }
+
+  @Example
+  void shouldTrancodeStringFieldNullRightPadding() {
+    final String pad = StringUtils.repeat('0', 99);
+    final StringHolderRightNull stringHolder = new StringHolderRightNull(null);
+    assertThat(Processor.trancode(stringHolder))
+        .isEqualTo(" " + pad);
+  }
+
+  @Example
+  void shouldTrancodeStringFieldNullLeftPadding() {
+    final String pad = StringUtils.repeat('0', 99);
+    final StringHolderLeftNull stringHolder = new StringHolderLeftNull(null);
+    assertThat(Processor.trancode(stringHolder))
+        .isEqualTo(pad + " ");
+  }
+
+  @Property
+  void shouldTrancodeStringFieldZeroPadRightPadding(@ForAll @StringLength(max = 99) final String string) {
+    final int length = string.length();
+    final String pad = StringUtils.repeat('0', 100 - length);
+    final StringHolderRightNull stringHolder = new StringHolderRightNull(string);
+    assertThat(Processor.trancode(stringHolder))
+        .isEqualTo(string + pad);
+  }
+
+  @Property
+  void shouldTrancodeStringFieldZeroPadLeftPadding(@ForAll @StringLength(max = 99) final String string) {
+    final int length = string.length();
+    final String pad = StringUtils.repeat('0', 100 - length);
+    final StringHolderLeftNull stringHolder = new StringHolderLeftNull(string);
+    assertThat(Processor.trancode(stringHolder))
+        .isEqualTo(pad + string);
+  }
+}
---
+++ src/test/java/br/com/tokunaga/trancoder/util/StringHolderLeft.java
@@ -0,0 +1,13 @@
+package br.com.tokunaga.trancoder.util;
+
+import br.com.tokunaga.trancoder.annotation.StringField;
+
+public class StringHolderLeft {
+
+  @StringField(size = 100, leftPad = true)
+  private String string;
+
+  public StringHolderLeft(final String string) {
+    this.string = string;
+  }
+}
